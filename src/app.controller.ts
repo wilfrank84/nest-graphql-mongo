6cd2dbe175e525644b7ec8e73fa6ffb5aa2f86d9
will <william.souza@raroprestador.com.br>
2023-07-20 15:02:53 -0300

    feat: add ws subscription and SSE

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
+import { Observable, interval, map } from 'rxjs';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map(() => ({ data: { hello: 'world' } }) as MessageEvent),
+    );
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AppController } from './app.controller';
 import { FooResolver } from './app.resolver';
+import { AppService } from './app.service';
 import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 import { Student } from './student/student.entity';
@@ -23,11 +25,20 @@ import { StudentModule } from './student/student.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
-      installSubscriptionHandlers: true,
+      // installSubscriptionHandlers: true,
+      subscriptions: {
+        'subscriptions-transport-ws': {
+          path: '/graphql',
+        },
+        'graphql-ws': {
+          path: '/graphql',
+        },
+      },
     }),
     LessonModule,
     StudentModule,
   ],
-  providers: [FooResolver],
+  controllers: [AppController],
+  providers: [AppService, FooResolver],
 })
 export class AppModule {}
